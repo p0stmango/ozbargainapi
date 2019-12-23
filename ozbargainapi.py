5db342ebdfee4ccc8e6053e587f5e9545bc49dbb
p0stman <30563815+p0stmango@users.noreply.github.com>
2019-12-23 12:47:54 +1100

    Added functionality to just use the entire feed

--- ozbargainapi.py
+++ ozbargainapi.py
@@ -6,20 +6,37 @@ parser = argparse.ArgumentParser(description="A tool to search the latest ozbarg
 parser.add_argument('searchstring')
 parser.add_argument('-n', '--number', type=int, default=5, help='Number of pages to search')
 args = parser.parse_args()
+
+def feedDownloader(number):
+	finalarray = []
+	for x in range(1,number):
+		if x ==1:
+			d = feedparser.parse('https://www.ozbargain.com.au/feed')
+		else:
+			d = feedparser.parse('https://www.ozbargain.com.au/feed/' + str(x))
+		for entry in d['entries']:
+			finalarray.append(entry.title + " | " + entry.link)
+#	print(finalarray)
+	return(finalarray)
 #print(args.searchstring)
 #if args.number:
 #    print(args.number)
 
-print("OZBARGAIN Search Results")
-matches = []
-for x in range(1,args.number):
-    if x == 1:
-        d = feedparser.parse('https://www.ozbargain.com.au/feed')
-    else:
-        d = feedparser.parse('https://www.ozbargain.com.au/feed/' + str(x))
-    for entry in d['entries']:
-        if args.searchstring.lower() in entry.title.lower():
-            matches.append(entry.title + " | " + entry.link)
-matches = list(dict.fromkeys(matches))
-for result in matches:
-    print(result)
+def searcher(searcharg):
+	print("OZBARGAIN Search Results")
+	matches = []
+	searchArray = feedDownloader(args.number)
+	for entry in searchArray:
+#		print(entry)
+		if searcharg.lower() in entry.lower():
+			matches.append(entry)
+	matches = list(dict.fromkeys(matches))
+	for result in matches:
+		print(result)
+
+if args.searchstring == "all":
+	searchArray = feedDownloader(args.number)
+	for entry in searchArray:
+		print(entry)
+else:
+	searcher(args.searchstring)
